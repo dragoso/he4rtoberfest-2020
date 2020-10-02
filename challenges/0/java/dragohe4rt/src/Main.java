692d71cd6d9e3b67d86a877fcd2c61d91e05e980
dragoso <42324767+dragoso@users.noreply.github.com>
2020-10-01 23:02:25 -0300

    Finalizando desafio

---
+++ challenges/0/java/dragohe4rt/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+
+    /**
+     * Inicialização comv ariaveis e argumentos encontrados na jvm
+     * @param args cadeia de strings informadas na inicialização da app
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+
+}
